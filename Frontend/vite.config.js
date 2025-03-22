f39d3fd378d055f8e31c4a216db2332e03c82e3e
Prashant Kumar <prashant63957174@gmail.com>
2025-03-23 03:14:42 +0530

    frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; // Import the Tailwind CSS plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(), // React plugin
+    tailwindcss(), // Tailwind CSS plugin
+  ],
+});
